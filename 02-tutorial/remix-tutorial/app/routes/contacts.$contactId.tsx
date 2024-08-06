ff4c2e63fabb7c10e9f3943d875456c0f796a7ab
kenkenpa198 <kenkenpa198@gmail.com>
2024-08-07 00:22:53 +0900

    complete: URL Params in Loaders

--- 02-tutorial/remix-tutorial/app/routes/contacts.$contactId.tsx
+++ 02-tutorial/remix-tutorial/app/routes/contacts.$contactId.tsx
@@ -1,18 +1,16 @@
-import { Form } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { getContact } from "../data";
 import type { FunctionComponent } from "react";
-
 import type { ContactRecord } from "../data";
 
+export const loader = async ({ params }) => {
+  const contact = await getContact(params.contactId);
+  return json({ contact });
+};
+
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar:
-      "https://kems-clew.net/assets/works/large/20221108_hello_mastodon.jpg",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
